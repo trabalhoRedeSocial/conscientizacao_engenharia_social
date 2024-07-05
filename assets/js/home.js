8198b4c79ddcfc514cb96516491f46f3178ff784
Eduardo <trabalhoredesocialproz@gmail.com>
2024-07-04 21:55:50 -0300

    concluida responsividade sidebar

--- assets/js/home.js
+++ assets/js/home.js
@@ -2,17 +2,19 @@
 
 //codigo para fechar e abrir a sidebar
 const hamburger_icon = document.querySelector('#hamburger-icon');
+const sidebar = document.querySelector('.sidebar');
+
 hamburger_icon.addEventListener('click', function showSidebar(){
-    const side_bar = document.querySelector('#sidebar');
-    if(side_bar.style.display.localeCompare('') == 0){
-        side_bar.style.display = 'flex'
+    const sidebar = document.querySelector('.sidebar');
+    if(sidebar.style.display.localeCompare('') == 0){
+        sidebar.style.display = 'flex'
     }
 })
 
-const close_sidebar_icon = document.querySelector('#close-sidebar');
+const close_sidebar_icon = document.querySelector('#close-icon');
 close_sidebar_icon.addEventListener('click', function closeSidebar(){
-    const side_bar = document.querySelector('#sidebar');
-    if(side_bar.style.display.localeCompare('flex') == 0){
-        side_bar.style.display = ''
+    const sidebar = document.querySelector('.sidebar');
+    if(sidebar.style.display.localeCompare('flex') == 0){
+        sidebar.style.display = ''
     }
 })
